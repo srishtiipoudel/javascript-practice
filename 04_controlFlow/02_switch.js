2920a25d45742babe5e9a4eb7a12d821677c3396
srishtiipoudel <160044917+srishtiipoudel@users.noreply.github.com>
2026-05-13 10:08:30 +0000

    switch case

---
+++ 04_controlFlow/02_switch.js
@@ -0,0 +1,31 @@
+// switch statement
+/*
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+
+const month = "march";
+
+switch (month) {
+  case "january":
+    console.log("January");
+    break;
+  case "february":
+    console.log("February");
+    break;
+  case "march":
+    console.log("March");
+    break;
+  case "april":
+    console.log("April");
+    break;
+  default:
+    console.log("default case match");
+    break;
+}
